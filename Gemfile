b971274963ca2a8530f2dd15bcd4690a4f11d53b
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-06-21 13:55:56 -0300

    Created seed with a task

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 0.18'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -22,6 +22,8 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+# A library for generating fake data such as names, addresses, and phone numbers.
+gem 'faker'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Gerando os tipos de contato (Kinds)...'
+Kind.create!([{description: "Amigo"},
+             {description: "Contato"},
+             {description: "Comercial"}])
+puts 'Gerando os tipos de contato (kinds)... [OK]'
+
+
+puts 'Gerando os tipos de contato (Contacts)...'
+  10.times do |i|
+    Contact.create!(
+         name: Faker::Name.name,
+         email: Faker::Internet.email,
+         kind: Kind.all.sample,
+         rmk: Faker::Lorem.paragraph([1,2,3,4,5].sample)
+    )
+  end
+puts 'Gerando os tipos de contato (Contacts)... [OK]'
